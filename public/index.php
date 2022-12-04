4d97c9499619ce235b97ce7b86f601bd778aa331
Nikita Serdashenko <nserdashenko@b2broker.com>
2022-12-04 16:24:42 +0400

    fixes

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/../src/Shared/functions/globalFunctions.php';
+require_once __DIR__ . '/../vendor/nserdashenko/mediocre/src/Shared/functions/globalFunctions.php';
 
 use Mediocre\Kernel\Kernel;
 
